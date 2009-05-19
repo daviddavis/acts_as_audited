f5f01ce7a56eeee138944fedf000a0b439193f25
David Davis <davidd@nfi-dt-113.netfriends.com>
2009-05-19 09:24:26 -0400

    Created new audit changes table to store changes.

--- generators/audited_migration/templates/migration.rb
+++ generators/audited_migration/templates/migration.rb
@@ -7,17 +7,30 @@ class <%= class_name %> < ActiveRecord::Migration
       t.column :user_type, :string
       t.column :username, :string
       t.column :action, :string
-      t.column :changes, :text
       t.column :version, :integer, :default => 0
       t.column :created_at, :datetime
+    end
+    
+    create_table :audit_changes, :force => true do |t|
+      t.column :audit_id, :integer
+      t.column :association_type, :string
+      t.column :association_id, :integer
+      t.column :action, :string
+      t.column :field, :string
+      t.column :old_value, :text
+      t.column :new_value, :text
     end
     
     add_index :audits, [:auditable_id, :auditable_type], :name => 'auditable_index'
     add_index :audits, [:user_id, :user_type], :name => 'user_index'
     add_index :audits, :created_at  
+    
+    add_index :audit_changes, [:audit_id, :association_type, :association_id], :name => 'audit_association_index'
+    add_index :audit_changes, [:audit_id], :name => 'audit_index'
   end
 
   def self.down
     drop_table :audits
+    drop_table :audit_changes
   end
 end
