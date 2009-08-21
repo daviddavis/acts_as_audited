38a08039479daa95181527ffe0e4f8fed8754d6d
David Davis <david@thirdlogic.net>
2009-08-21 08:42:36 -0400

    Explicitly converting values in AuditChange to strings so the oracle adapter won't blow up.

--- lib/acts_as_audited.rb
+++ lib/acts_as_audited.rb
@@ -170,7 +170,7 @@ module CollectiveIdea #:nodoc:
         
         def audited_changes(all = false)
           changed_attributes.except(*non_audited_columns).inject([]) do |changes,(attr, old_value)|
-            changes << AuditChange.new(:field => attr, :old_value => old_value, :new_value => self[attr])
+            changes << AuditChange.new(:field => attr, :old_value => old_value.to_s, :new_value => self[attr].to_s)
             changes
           end     
         end
